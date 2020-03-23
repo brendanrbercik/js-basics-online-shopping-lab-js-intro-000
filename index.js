6a97b0696bca204e3ba5decce83afb8743499787
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-23 18:18:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,4 +59,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if exists(cardNumber) {
   return("Your total cost is " + total().total + ", which will be charged to the card " + cardNumber +".")
-}
+  }
+  else {
+    return('Sorry, we don\'t have a credit card on file for you.');
+  }
