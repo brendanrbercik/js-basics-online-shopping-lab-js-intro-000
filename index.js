67a90fa43f4c5837b22ceffa2c0009d8cbd8a2a2
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-23 18:06:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     if (item == cart[i].itemName) {
-      cart = cart.splice(i,1);
+      cart.splice(i,1);
       return cart;
     }
   }
