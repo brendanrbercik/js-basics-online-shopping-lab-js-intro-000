ad93ae7324498825dc8922cd8e84a010f3df00ec
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-23 18:15:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,5 +60,6 @@ function placeOrder(cardNumber) {
   if (cardNumber == false) {
   return('Sorry, we don\'t have a credit card on file for you.');
   }
-  return("Your total cost is " + total() + ", which will be charged to the card " + cardNumber +".")
+  total();
+  return("Your total cost is " + total().total + ", which will be charged to the card " + cardNumber +".")
 }
