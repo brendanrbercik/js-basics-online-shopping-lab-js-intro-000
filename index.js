5fc24c2974e8d6012482b18394f466536e4294d0
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-23 18:21:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,8 +40,9 @@ function viewCart() {
 }
 
 function total() {
+  var total;
   for (let i = 0; i < cart.length; i++) {
-    total = total + cart[i].itemPrice;
+  total = total + cart[i].itemPrice;
   }
   return (total);
 }
