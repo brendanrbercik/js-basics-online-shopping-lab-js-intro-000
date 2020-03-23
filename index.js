c013392a9940229dd8149f1bb52e5263d1228821
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-23 18:19:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,8 +58,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber !== undefined) {
-  return("Your total cost is " + total().total + ", which will be charged to the card " + cardNumber +".")
+    return("Your total cost is " + total().total + ", which will be charged to the card " + cardNumber +".")
   }
   else {
     return('Sorry, we don\'t have a credit card on file for you.');
   }
+}
